87773da79a5080519717dd77f231d2e73887173a
Lenol <ek3434@naver.com>
2021-12-24 00:06:50 +0900

    1213

--- Leetcode/238/이필웅/238.py
+++ Leetcode/238/이필웅/238.py
@@ -1,6 +1,7 @@
 # LeetCode 238. Product of Array Except Self
 # https://leetcode.com/problems/product-of-array-except-self/submissions/
 
+from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         p = 1
