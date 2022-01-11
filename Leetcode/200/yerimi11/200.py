6d4bbe0a86ec707f96eda3e09dec44d2265d2d03
yerim <sontt4266@naver.com>
2022-01-11 21:54:11 +0900

    0111

--- Leetcode/200/yerimi11/200.py
+++ Leetcode/200/yerimi11/200.py
@@ -5,20 +5,18 @@ class Solution(object):
         :rtype: int
         """
         def dfs(grid, i, j):
-            if 0<=i<m and 0<=j<n and grid[i][j] == "1":
-                grid[i][j] = "2"
-                dfs(grid, i-1, j)
+            if 0<=i<m and 0<=j<n and grid[i][j] == '1':
+                grid[i][j] = '2'
                 dfs(grid, i+1, j)
-                dfs(grid, i, j-1)
+                dfs(grid, i-1, j)
                 dfs(grid, i, j+1)
-        if not grid:
-            return 0
-        
+                dfs(grid, i, j-1)
+                
         m, n, count = len(grid), len(grid[0]), 0
+            
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
+                if grid[i][j] == '1':
                     count += 1
                     dfs(grid, i, j)
-        
         return count
